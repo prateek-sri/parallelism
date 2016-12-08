9df96ff32d1b133c4e3433476a3cc9d3615e696c
Rohit Aggarwal <ra33932@login-knl1.stampede.tacc.utexas.edu>
2016-12-07 19:56:54 -0600

    updated stencil for cache blocking

--- benchmarks/stencil/src/omp_base/kernels.c
+++ benchmarks/stencil/src/omp_base/kernels.c
@@ -11,27 +11,27 @@
 void cpu_stencil(float c0,float c1, float *A0,float * Anext,const int nx, const int ny, const int nz)
 {
 
-  int i;  
-  #pragma omp parallel for
-	for(i=1;i<nx-1;i++)
-	{
-    int j,k;
-		for(j=1;j<ny-1;j++)
-		{
-			for(k=1;k<nz-1;k++)
-			{
-  //i      #pragma omp critical
-				Anext[Index3D (nx, ny, i, j, k)] = 
-				(A0[Index3D (nx, ny, i, j, k + 1)] +
-				A0[Index3D (nx, ny, i, j, k - 1)] +
-				A0[Index3D (nx, ny, i, j + 1, k)] +
-				A0[Index3D (nx, ny, i, j - 1, k)] +
-				A0[Index3D (nx, ny, i + 1, j, k)] +
-				A0[Index3D (nx, ny, i - 1, j, k)])*c1
-				- A0[Index3D (nx, ny, i, j, k)]*c0;
-			}
-		}
-	}
+    int i;  
+#pragma omp parallel for
+    for(i=1;i<nx-1;i++)
+    {
+        int j,k;
+        for(j=1;j<ny-1;j++)
+        {
+            for(k=1;k<nz-1;k++)
+            {
+                //i      #pragma omp critical
+                Anext[Index3D (nx, ny, i, j, k)] = 
+                    (A0[Index3D (nx, ny, i, j, k + 1)] +
+                     A0[Index3D (nx, ny, i, j, k - 1)] +
+                     A0[Index3D (nx, ny, i, j + 1, k)] +
+                     A0[Index3D (nx, ny, i, j - 1, k)] +
+                     A0[Index3D (nx, ny, i + 1, j, k)] +
+                     A0[Index3D (nx, ny, i - 1, j, k)])*c1
+                    - A0[Index3D (nx, ny, i, j, k)]*c0;
+            }
+        }
+    }
 
 }
 
