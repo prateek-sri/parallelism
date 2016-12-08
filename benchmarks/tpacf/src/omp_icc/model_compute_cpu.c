373f7050a60ccebd445b8fa98119372d996f21f7
Rohit Aggarwal <ra33932@c567-104.stampede.tacc.utexas.edu>
2016-12-08 16:37:33 -0600

    tpacf omp_icc improved

--- benchmarks/tpacf/src/omp_icc/model_compute_cpu.c
+++ benchmarks/tpacf/src/omp_icc/model_compute_cpu.c
@@ -8,10 +8,12 @@
 #include <sys/time.h>
 #include <string.h>
 #include <math.h>
-#include <stdio.h> 
+#include <stdio.h>
+#include <omp.h>
 
 #include "model.h"
 
+#define LEN 1048576 
 int doCompute(struct cartesian *data1, int n1, struct cartesian *data2, 
         int n2, int doSelf, long long *data_bins, 
         int nbins, float *binb)
@@ -25,12 +27,8 @@ int doCompute(struct cartesian *data1, int n1, struct cartesian *data2,
     }
 #pragma omp parallel for 
     for (ii =0; ii < nbins+2; ii++)
-    {
         for (j = 0; j < 256; j++)
-        {
             data_bins1[j][ii] = 0;
-        }
-    }
 
     for (i = 0; i < ((doSelf) ? n1-1 : n1); i++)
     {
@@ -38,44 +36,35 @@ int doCompute(struct cartesian *data1, int n1, struct cartesian *data2,
         const register float yi = data1[i].y;
         const register float zi = data1[i].z;
 
-	#pragma omp parallel
+#pragma omp parallel for      
+        for (j = (doSelf) ? i+1 : 0; j < n2; j++)
         {
-            int num_thd = omp_get_num_threads ();
             int tid = omp_get_thread_num ();
-	    
-	    #pragma omp for      
-	    for (j = ((doSelf) ? i+1 : 0); j < n2; j++)
+            register float dot = xi * data2[j].x + yi * data2[j].y + 
+                zi * data2[j].z;
+
+            // run binary search
+            register int min = 0;
+            register int max = nbins + 2;
+            register int k, indx;
+
+            while (max > min + 1)
             {
-                register float dot = xi * data2[j].x + yi * data2[j].y + 
-                    zi * data2[j].z;
-
-                // run binary search
-                register int min = 0;
-                register int max = nbins + 2;
-                register int k, indx;
-
-                while (max > min + 1)
-                {
-                    k = (min + max) / 2;
-                    if (dot >= binb[k]) 
-                        max = k;
-                    else 
-                        min = k;
-                }
-                //#pragma omp atomic	  
-                data_bins1[tid][max-1] += 1; /*k = max;*/ 
-
+                k = (min + max) / 2;
+                if (dot >= binb[k]) 
+                    max = k;
+                else 
+                    min = k;
             }
+            //#pragma omp atomic	  
+            data_bins1[tid][max-1] += 1; /*k = max;*/ 
         }
     }
 #pragma omp parallel for
-            for (ii =0; ii < nbins+2; ii++)
-            {
-                for (j = 0; j < 256; j++)
-                {
-                    data_bins[ii]+=data_bins1[j][ii];
-                }
-            }
+    for (ii =0; ii < nbins+2; ii++)
+        for (j = 0; j < 256; j++)
+            data_bins[ii]+=data_bins1[j][ii];
+    
     return 0;
 }
 
